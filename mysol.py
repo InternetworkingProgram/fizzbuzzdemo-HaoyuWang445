0d0cd2a41ef7b61028c1ba6edcb383aa510542a0
HaoyuWang445 <113454202+HaoyuWang445@users.noreply.github.com>
2022-09-20 15:11:41 -0300

    Update mysol.py

--- mysol.py
+++ mysol.py
@@ -7,6 +7,6 @@ def myFizzBuzzSol():
 		print {
 			3: "FIzz",
 			5: "Buzz",
-			15: "FizzBuz"}.get(15*(not i%15)or 5*(not i%5) or 3*(not							 i%3), '{}'.format(i))
+			15: "FizzBuz"}.get(15*(not i%15)or 5*(not i%5) or 3*(not i%3), '{}'.format(i))
 
 myFizzBuzzSol() 
