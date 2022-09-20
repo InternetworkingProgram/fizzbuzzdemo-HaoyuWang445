c11d3e35cede611e017d3918f027cf370c8d6b98
Jamie Wang <JamieWang@JamiedeMacBook-Pro.local>
2022-09-20 15:01:53 -0300

    Final version with reference

--- mysol.py
+++ mysol.py
@@ -1,5 +1,12 @@
-print ("Number frim 1 to 100: ")
+#Ref: https://stackoverflow.com/questions/22743860/python-fizzbuzz
+#Note I changed the function name too!
 
-for n in range (1, 101):
+def myFizzBuzzSol():
 
-	print(n)
+	for i in range(1,101):
+		print {
+			3: "FIzz",
+			5: "Buzz",
+			15: "FizzBuz"}.get(15*(not i%15)or 5*(not i%5) or 3*(not							 i%3), '{}'.format(i))
+
+myFizzBuzzSol() 
